a8344d40777e2733a8beca68145f29781f2a48ac
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2014-12-23 16:23:26 +0100

    third problem solved

---
+++ python/atoitoa.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+
+def atoi(s):
+    return reduce(lambda x, y: x * 10 + y, [ord(d) - ord('0') for d in s], 0)
+
+
+def itoa(n):
+    digits = []
+    while n > 0:
+        digits = [n % 10] + digits
+        n /= 10
+
+    return ''.join([chr(d + ord('0')) for d in digits])
+
+
+def Usage(name):
+    print "Usage: %s <n>" % name
+
+
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        Usage(sys.argv[0])
+        exit(1)
+    s = sys.argv[1]
+    n = atoi(s)
+    if n != int(s):
+        print "Error!"
+
+    s2 = itoa(n)
+    if s != s2:
+        print "Error2"
+
+    print "%s => %d => %s" % (s, n, s2)
